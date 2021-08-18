b51538e7e4c935b7bc10d9be7ca548e56ac6eb74
lourivanrluz <lourivan-rodrigues@hotmail.com>
2021-08-18 17:35:01 -0300

    apresentaçao result

--- src/javaScript/index.js
+++ src/javaScript/index.js
@@ -1,28 +1,21 @@
 function display20Flips() {
     const results = [];
-    for(let count = 0; count <20; count ++){
+    const telaResult = document.createElement('p')
+    telaResult.innerHTML = 'Resultado:</br>'
 
+    for(let count = 0; count <20; count ++){
         coin.flip()
         const result = coin.state
         results.push(result)
+        telaResult.innerHTML += ` ${coin.toString()}`
     }
+    document.body.appendChild(telaResult)
+
     return results
-    // Use um loop para arremessar a moeda 20 vezes.
-    // Depois que o seu loop terminar, exiba o resultado na página no formato de TEXTO.
-    // Além de exibir os resultados na página, não esqueça
-    // de retornar o valor de "results".
-    // Caso esqueça de retornar "results", sua função não
-    // irá passar nos testes.
   }
   
   function display20Images() {
     const results = [];
-    // Use um loop para arremessar a moeda 20 vezes.
-    // Depois que o seu loop terminar, exiba o resultado na página no formato de IMAGEM.
-    // Além de exibir os resultados na página, não esqueça
-    // de retornar o valor de "results".
-    // Caso esqueça de retornar "results", sua função não
-    // irá passar nos testes.
     const telaResult = document.createElement('div')
     telaResult.classList.add('result')
 
@@ -37,3 +30,6 @@ function display20Flips() {
     return results
 
   }
+
+  display20Images()
+  display20Flips()
